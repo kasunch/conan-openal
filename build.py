68b98823f6723b177d230bde58513f39906eb438
Kasun Hewage <kasun.ch@gmail.com>
2020-03-15 18:16:00 +0100

    new updates

--- build.py
+++ build.py
@@ -5,5 +5,5 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(username="_", channel="_", build_policy="outdated")
+    builder = build_template_default.get_builder(build_policy="outdated")
     builder.run()
